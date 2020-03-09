ec7df514baf0736c30eeb18ea6f3b54e6b51c2ec
BBR-webdev <bjoernbreski@web.de>
2020-03-09 07:10:39 +0100

    initial commit of BreakoutClone project

---
+++ BreakoutClone/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    // Ball movement speed 
+    public float speed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // method to calculate hit Factor for angled shots
+
+        float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+        {
+            // ascii art:
+            //
+            // 1  -0.5  0  0.5   1  <- x value
+            // ===================  <- racket
+            //
+
+            return (ballPos.x - racketPos.x) / racketWidth;
+
+            
+
+        }
+
+        // Check if collider is racket and if true, execute
+        if (collision.gameObject.name == "racket")
+        {
+            // Feed data where ball was hit: and calculate the fireback angle
+            // transform.position is ball position in worldspace. 
+            // collision.transform.position is position of the part of the racket that hit
+            // collider.bounds.size.x is the width ofthe racket
+            float angleCalc = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 direction = new Vector2(angleCalc, 1).normalized;
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+        }
+    }
+
+}
---
+++ BreakoutClone/Assets/Scripts/RacketMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour
+{
+    // variables for racket movement speed and direction/orientation
+    public float speed = 30;
+    public string axis = "Horizontal";
+
+    // Function that updates on fixed time interval; includes velocity calculation for the racket
+    void FixedUpdate()
+    {
+        float direction = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction, 0) * speed;
+    }
+
+
+}
